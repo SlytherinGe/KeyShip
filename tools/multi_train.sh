e174d76e1d8fd671ca9b6dc74e2803c666987f57
slytheringe <slytheringe@qq.com>
2022-06-10 21:36:37 +0800

    add benchmark r3det and s2anet for ssdd

--- tools/multi_train.sh
+++ tools/multi_train.sh
@@ -1,15 +1,15 @@
 echo "rotated_retinanet_obb_gwd_r50_fpn_ssdd_oc"
-python tools/train.py configs/gwd/rotated_retinanet_obb_gwd_r50_fpn_ssdd_oc.py --seed 42
+python tools/train.py configs/benchmark_ssdd/S2ANet.py --seed 42
 wait
 echo "r3det_refine_r50_fpn_1x_ssdd_oc"
-python tools/train.py configs/r3det/r3det_refine_r50_fpn_1x_ssdd_oc.py --seed 42
+python tools/train.py configs/benchmark_ssdd/r3det.py --seed 42
 wait
-echo "rotated_faster_rcnn_r50_oc"
-python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_oc.py --seed 42
-wait
-echo "rotated_retinanet_obb_r50_fpn_oc"
-python tools/train.py configs/rotated_retinanet/rotated_retinanet_obb_r50_fpn_oc.py --seed 42
-wait
+# echo "rotated_faster_rcnn_r50_oc"
+# python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_oc.py --seed 42
+# wait
+# echo "rotated_retinanet_obb_r50_fpn_oc"
+# python tools/train.py configs/rotated_retinanet/rotated_retinanet_obb_r50_fpn_oc.py --seed 42
+# wait
 # python tools/train.py configs/extreme_ship/extreme_ship_hourglass.py --seed 42
 # wait
 # python tools/train.py configs/extreme_ship/backup.py --seed 42
