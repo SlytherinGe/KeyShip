22be51a7b9ec31bbf6c4cfb836b99735e7f99a75
slytheringe <slytheringe@qq.com>
2022-06-11 22:15:13 +0800

    update benchmark gliding_vertex, oriented_reppoints

--- tools/multi_train.sh
+++ tools/multi_train.sh
@@ -1,8 +1,8 @@
-echo "rotated_retinanet_obb_gwd_r50_fpn_ssdd_oc"
-python tools/train.py configs/benchmark_ssdd/S2ANet.py --seed 42
+echo "configs/benchmark_ssdd/gliding_vertex.py"
+python tools/train.py configs/benchmark_ssdd/gliding_vertex.py --seed 42
 wait
-echo "r3det_refine_r50_fpn_1x_ssdd_oc"
-python tools/train.py configs/benchmark_ssdd/r3det.py --seed 42
+echo "configs/benchmark_ssdd/oriented_reppoints.py"
+python tools/train.py configs/benchmark_ssdd/oriented_reppoints.py --seed 42
 wait
 # echo "rotated_faster_rcnn_r50_oc"
 # python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_oc.py --seed 42
