9018220637121022201b306a156d12058992a2e1
SlytherinGe <slytheringe@qq.com>
2022-07-02 15:13:29 +0800

    update benchmark for rsdd

--- tools/multi_train.sh
+++ tools/multi_train.sh
@@ -1,9 +1,9 @@
-echo "configs/benchmark_ssdd/gliding_vertex.py"
-python tools/train.py configs/benchmark_ssdd/gliding_vertex.py --seed 42
-wait
-echo "configs/benchmark_ssdd/oriented_reppoints.py"
-python tools/train.py configs/benchmark_ssdd/oriented_reppoints.py --seed 42
-wait
+# echo "configs/benchmark_ssdd/gliding_vertex.py"
+# python tools/train.py configs/benchmark_ssdd/gliding_vertex.py --seed 42
+# wait
+# echo "configs/benchmark_ssdd/oriented_reppoints.py"
+# python tools/train.py configs/benchmark_ssdd/oriented_reppoints.py --seed 42
+# wait
 # echo "rotated_faster_rcnn_r50_oc"
 # python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_oc.py --seed 42
 # wait
@@ -16,3 +16,10 @@ wait
 # wait
 # python tools/train.py configs/extreme_ship/backup1.py --seed 42
 # wait
+
+bash tools/dist_train.sh configs/benchmark_rsdd/S2ANet.py 2 --seed 42
+wait
+bash tools/dist_train.sh configs/benchmark_rsdd/roi_transformer.py 2 --seed 42
+wait
+bash tools/dist_train.sh configs/benchmark_rsdd/oriented_rcnn.py 2 --seed 42
+wait
